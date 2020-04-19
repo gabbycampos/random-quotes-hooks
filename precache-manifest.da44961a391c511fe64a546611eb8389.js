e7ea5744013b3b2cbd05269c962c929cdd76ea70
Gabby Campos <gabbycampos@Gabbys-MacBook-Pro.local>
2020-04-19 14:22:07 -0500

    Updates

---
+++ precache-manifest.da44961a391c511fe64a546611eb8389.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26cbe73c14bd37a7ca1413b4ab5ad64",
+    "url": "/random-quotes-hooks/index.html"
+  },
+  {
+    "revision": "204d77176dad4b0b5928",
+    "url": "/random-quotes-hooks/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "9cbf81a57cea23a792d7",
+    "url": "/random-quotes-hooks/static/css/main.9b19ca14.chunk.css"
+  },
+  {
+    "revision": "204d77176dad4b0b5928",
+    "url": "/random-quotes-hooks/static/js/2.98c989bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quotes-hooks/static/js/2.98c989bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cbf81a57cea23a792d7",
+    "url": "/random-quotes-hooks/static/js/main.8664088d.chunk.js"
+  },
+  {
+    "revision": "89159aa1944e14d50f62",
+    "url": "/random-quotes-hooks/static/js/runtime-main.748f1499.js"
+  }
+]);
